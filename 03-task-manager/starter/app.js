a594a4a167aad1fdc00cd726e25d8ee9cfeb6fd4
Christopher Van Kampen <christopheryungvankampen@gmail.com>
2024-12-12 15:36:00 -0500

    Added Input validation, Error Handling, CRUD Ops

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,24 +3,24 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
-
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
 
-app.use(express.json ())
+app.use(express.static('./public'))
+app.use(express.json())
 
 
 //routes
-app.get('/hello', (req, res) => (
-    res.send('Task Manager App')
-))
 
-app.use('/api/v1/tasks',tasks)
+app.use('/api/v1/tasks', tasks)
 
 
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 
